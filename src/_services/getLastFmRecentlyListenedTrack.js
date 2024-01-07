841c82c34dc212381162f5ac1dbed33673bbea69
hyoaru <jjcabreraaaa@gmail.com>
2024-01-07 20:47:08 +0800

    [modify] api url removed protocol

--- src/_services/getLastFmRecentlyListenedTrack.js
+++ src/_services/getLastFmRecentlyListenedTrack.js
@@ -1,6 +1,6 @@
 export default async function getLastFmRecentlyListenedTrack() {
   const lastfmApiKey = import.meta.env.VITE_LAST_FM_API_KEY
-  const apiUrl = 'http://ws.audioscrobbler.com/2.0'
+  const apiUrl = '//ws.audioscrobbler.com/2.0'
   const apiMethod = 'user.getrecenttracks&user=hyoaru'
   const data = await fetch(`${apiUrl}/?method=${apiMethod}&api_key=${lastfmApiKey}&format=json`, {
     method: "GET",
