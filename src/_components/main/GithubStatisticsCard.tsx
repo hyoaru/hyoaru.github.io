2c8c16f47109d2842a7b049bca90419a0c6fb684
hyoaru <jjcabreraaaa@gmail.com>
2024-01-31 21:36:27 +0800

    [add] moving border

--- src/_components/main/GithubStatisticsCard.tsx
+++ src/_components/main/GithubStatisticsCard.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from '@nextui-org/react'
+import { Button as MovingBorder } from '@components/ui/MovingBorder'
 
 type GithubStatisticsCardProps = {
   label: string
@@ -11,10 +12,16 @@ export default function GithubStatisticsCard({ label, value, isLoading, isError 
   return (
     <>
       <Skeleton isLoaded={!isLoading} classNames={{ content: 'w-full h-full', base: 'flex rounded-xl w-full h-full' }}>
-        <div className=" text-center bg-background border rounded-xl flex flex-col justify-center align-center w-full h-full md:py-4 dark:bg-foreground dark:text-background">
-          <p className="font-semibold md:text-sm lg:text-lg">{isLoading || isError ? '-' : value?.toLocaleString()}</p>
-          <p className="text-xs md:text-[8px] lg:text-[10px]">{label}</p>
-        </div>
+        <MovingBorder
+          borderRadius="0.75rem"
+          className="bg-white w-full h-full dark:bg-background text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          disabled
+        >
+          <div className=" text-center bg-background rounded-xl flex flex-col justify-center align-center w-full h-full md:py-4 dark:bg-foreground dark:text-background">
+            <p className="font-semibold md:text-sm lg:text-lg">{isLoading || isError ? '-' : value?.toLocaleString()}</p>
+            <p className="text-xs md:text-[8px] lg:text-[10px]">{label}</p>
+          </div>
+        </MovingBorder>
       </Skeleton>
     </>
   )
--- src/_components/main/TechnologiesCardGroup.tsx
+++ src/_components/main/TechnologiesCardGroup.tsx
@@ -6,6 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 import Marquee from '@components/ui/Marquee'
 import getTechnologies from '@services/main/getTechnologies'
 import TechnologyBadge from '@components/main/TechnologyBadge'
+import { Button as MovingBorder } from '@components/ui/MovingBorder'
 
 export default function TechnologiesCardGroup() {
 
@@ -24,13 +25,19 @@ export default function TechnologiesCardGroup() {
     <>
       <div className="grid grid-cols-12 gap-4 md:gap-6 lg:gap-8">
         <div className="col-span-12 md:col-span-5 xl:col-span-4">
-          <div className="rounded-xl bg-background p-4 flex items-center justify-center gap-4 h-full border dark:border-default">
-            <Cpu className='text-primary' />
-            <div className="">
-              <p className='font-semibold text-xs md:text-sm dark:text-primary'>Languages & Technologies</p>
-              <p className='text-[10px]'>Technologies I work and used to work with</p>
+          <MovingBorder
+            borderRadius="0.75rem"
+            className="bg-white w-full dark:bg-background text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            disabled
+          >
+            <div className="p-4 flex items-center justify-center gap-4 h-full">
+              <Cpu className='text-primary' />
+              <div className="">
+                <p className='font-semibold text-xs md:text-sm dark:text-primary'>Languages & Technologies</p>
+                <p className='text-[10px]'>Technologies I work and used to work with</p>
+              </div>
             </div>
-          </div>
+          </MovingBorder>
         </div>
         <div className="col-span-12 md:col-span-7 xl:col-span-8">
           <Skeleton classNames={{ base: `w-full h-full ${isLoading && 'rounded-xl'}`, content: 'w-full h-full' }} isLoaded={!isLoading}>
