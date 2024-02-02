f8164bf066c7ba5a0df05dea3e4ae905a94eb0c3
hyoaru <jjcabreraaaa@gmail.com>
2024-02-02 20:49:06 +0800

    [fix] social card only partly clickable

--- src/_components/about/SocialCard.tsx
+++ src/_components/about/SocialCard.tsx
@@ -3,14 +3,14 @@ import { SocialType } from "@constants/about/types"
 
 export default function SocialCard({ link, name, label }: SocialType) {
   return (
-    <BentoBox className='!p-5'>
-      <a href={link} target='_blank' className={`flex flex-grow items-center gap-4 break-inside-avoid-column ${!link && 'pointer-events-none'}`}>
+    <a href={link} target='_blank' className={!link ? 'pointer-events-none' : ''}>
+      <BentoBox className='!p-5 flex flex-grow items-center gap-4 break-inside-avoid-column'>
         <img width={30} height={30} src={`https://cdn.simpleicons.org/${name}/000000/ffffff`} alt="" />
         <div className="">
           <p className="text-xs opacity-50">Social</p>
           <p className="font-bold text-sm">{label}</p>
         </div>
-      </a>
-    </BentoBox>
+      </BentoBox>
+    </a>
   )
 }
