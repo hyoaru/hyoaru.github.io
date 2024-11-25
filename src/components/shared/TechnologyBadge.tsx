2d5ba989ff093881ca2c3dba4662c2881460db1d
hyoaru <jjcabreraaaa@gmail.com>
2024-11-25 13:58:38 +0800

    [fix] colors

--- src/components/shared/TechnologyBadge.tsx
+++ src/components/shared/TechnologyBadge.tsx
@@ -3,20 +3,32 @@ import { cn, Image } from "@nextui-org/react";
 type TechnologyBadgeProps = {
   children: React.ReactNode;
   className?: string;
-}
+};
 
 type TechnologyBadgeLogoProps = {
   logo: string;
-}
+};
 
-export const TechnologyBadge = ({ children, className }: TechnologyBadgeProps) => (
-  <div className={cn("rounded-lg bg-custom-background px-4 py-2 font-bold flex items-center gap-2", className)} >
+export const TechnologyBadge = ({
+  children,
+  className,
+}: TechnologyBadgeProps) => (
+  <div
+    className={cn(
+      "rounded-lg bg-custom-secondary px-4 py-2 font-bold flex items-center gap-2",
+      className,
+    )}
+  >
     {children}
   </div>
-)
+);
 
 const TechnologyBadgeLogo = ({ logo }: TechnologyBadgeLogoProps) => (
-  <Image width={14} height={14} src={`https://cdn.simpleicons.org/${logo}/000000/0070f0`} />
-)
+  <Image
+    width={14}
+    height={14}
+    src={`https://cdn.simpleicons.org/${logo}/000000/0070f0`}
+  />
+);
 
-TechnologyBadge.Logo = TechnologyBadgeLogo
+TechnologyBadge.Logo = TechnologyBadgeLogo;
