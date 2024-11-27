4fe9638135deeaf140cc91cab80211909c552027
hyoaru <jjcabreraaaa@gmail.com>
2024-11-27 21:23:11 +0800

    [add] workaround for sticky like behavior to tablish

--- src/sections/Content.tsx
+++ src/sections/Content.tsx
@@ -7,13 +7,13 @@ import Certifications from "./Certifications";
 export default function Content() {
   return (
     <>
-      <div className="flex h-full flex-col space-y-2">
+      <div className="flex h-[90vh] flex-col space-y-2 overflow-y-scroll lg:h-full">
         <Tabs
           aria-label="Options"
           classNames={{
-            tabList: " w-max sticky",
+            tabList: " w-max",
             base: "w-full bg-custom-secondary border-0 rounded-xl m-0 p-0",
-            tab: "bg-background rounded-lg opacity-100 px-3 h-8",
+            tab: "bg-background rounded-lg opacity-100 px-3 h-8 sticky top-0",
             tabContent:
               "group-data-[selected=true]:text-primary-foreground text-sm 2xl:text-base text-foreground font-bold",
             cursor: "bg-primary",
