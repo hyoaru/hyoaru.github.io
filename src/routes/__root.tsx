14a4b4995f7d31776f86e8258e6c60d1ed2c96f2
hyoaru <jjcabreraaaa@gmail.com>
2024-11-28 22:12:15 +0800

    [update] remove router dev tools on production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,22 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "sonner";
+import React from "react";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  const TanStackRouterDevtools =
+    process.env.NODE_ENV === "production"
+      ? () => null
+      : React.lazy(() =>
+          import("@tanstack/router-devtools").then((res) => ({
+            default: res.TanStackRouterDevtools,
+          })),
+        );
+
   return (
     <>
       <div className="">
