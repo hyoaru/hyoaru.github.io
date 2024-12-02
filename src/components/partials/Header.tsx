4498ddf2a94e3b6a529e6182fe2007686edaa3d9
hyoaru <jjcabreraaaa@gmail.com>
2024-12-02 11:24:03 +0800

    [fix] resume modal not showing on small screens

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -9,13 +9,13 @@ export default function Header() {
 
   return (
     <>
-      <div className="flex items-center rounded-xl bg-background p-[3px] text-sm sm:text-base lg:text-sm xl:text-base">
+      <div className="relative flex items-center rounded-xl bg-background p-[3px] text-sm sm:text-base lg:text-sm xl:text-base">
         <PersonStanding className="size-8 rounded-xl p-1" />
         <p className="me-auto font-bold">Cabrera, Jen Jade</p>
 
         <div className="flex gap-1">
           <Button
-            onPress={onOpen}
+            onClick={onOpen}
             isIconOnly
             className={cn(
               "flex h-8 rounded-lg text-sm font-bold sm:hidden sm:text-base lg:text-sm 2xl:gap-4 2xl:text-base",
