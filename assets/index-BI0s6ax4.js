a713916ef12e9df81307ea45fb5f59ae849b009b
hyoaru <jjcabreraaaa@gmail.com>
2025-09-01 20:29:57 +0800

    Compressed certification images to 1% quality

--- assets/index-BI0s6ax4.js
+++
@@ -1,1 +0,0 @@
-import{a3 as a}from"./features-animation-CVX-m0Pl.js";import"./index-7cONq9WC.js";var i=a;export{i as default};
---
+++ assets/src-UW24ZMRV-B0YkSZz0.js
@@ -0,0 +1,1 @@
+import{a3 as a}from"./features-animation-CQaJdZG8.js";import"./index-D7Nx9_6I.js";var t=a;export{t as default};
