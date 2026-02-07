f017590654d4450f3635fa59959c977b6601f051
hyoaru <jjcabreraaaa@gmail.com>
2026-02-08 01:11:43 +0800

    feat(ui): update home layout with ScrollShadow and background enhancements
    - Add ScrollShadow to sidebar for improved scrolling UX
    - Refine background styles with bg-dots and custom background color
    - Remove redundant height and width styles from bg-grid and bg-dots
    - Clean up global CSS for layout consistency

--- src/core/router/routes/index.tsx
+++ src/core/router/routes/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/core";
 import { GitActivity, GitStats } from "@/features/git";
 import { Profile, RecentJob } from "@/features/profile";
 import { Technologies } from "@/features/technologies";
+import { ScrollShadow } from "@heroui/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -10,17 +11,19 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div className="mx-auto max-w-[1680px] p-5">
-      <div className="flex gap-5">
-        <div className="h-fit w-[38%] shrink-0 space-y-2.5">
-          <Header />
-          <Profile />
-          <RecentJob />
-          <Technologies />
-          <GitStats />
-          <GitActivity />
+    <div className="bg-dots absolute flex h-full w-full items-center justify-center">
+      <div className="bg-custom-background mx-auto h-full max-h-[1080px] w-full max-w-[1920px] rounded-xl p-5">
+        <div className="flex h-full w-full gap-5">
+          <ScrollShadow hideScrollBar className="w-[38%] shrink-0 space-y-2.5">
+            <Header />
+            <Profile />
+            <RecentJob />
+            <Technologies />
+            <GitStats />
+            <GitActivity />
+          </ScrollShadow>
+          <div className="h-fit w-full"></div>
         </div>
-        <div className="h-fit w-full"></div>
       </div>
     </div>
   );
