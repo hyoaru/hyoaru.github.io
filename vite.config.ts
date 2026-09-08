7f7e11e858ea13773cbe6c35b4df71941050ca7a
hyoaru <the.jadecabrera@gmail.com>
2026-09-09 00:38:10 +0800

    chore: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,11 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/infrastructure/router/routes",
+      generatedRouteTree: "./src/infrastructure/router/routeTree.gen.ts",
+    }),
   ],
 });
