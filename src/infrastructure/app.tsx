1928ebff9e18d4acebf197cbb40c3f1b628300e9
hyoaru <the.jadecabrera@gmail.com>
2026-09-09 08:04:38 +0800

    chore: add tanstack

--- src/infrastructure/app.tsx
+++ src/infrastructure/app.tsx
@@ -1,7 +1,38 @@
-export const App = () => {
-  return (
-    <>
-      <p className="">test</p>
-    </>
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "@tanstack/react-router";
+import { ThemeProvider } from "next-themes";
+import { useState } from "react";
+import { router } from "./router";
+
+export default function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000 * 60,
+            refetchOnWindowFocus: true,
+          },
+        },
+      }),
   );
-};
+
+  return (
+    <ThemeProvider enableSystem attribute="data-theme" defaultTheme="system">
+      <QueryClientProvider client={queryClient}>
+        <InnerApp />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
+
+function InnerApp() {
+  const queryClient = useQueryClient();
+  return <RouterProvider router={router} context={{ queryClient }} />;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
       routesDirectory: "./src/infrastructure/router/routes",
       generatedRouteTree: "./src/infrastructure/router/routeTree.gen.ts",
     }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
 });
